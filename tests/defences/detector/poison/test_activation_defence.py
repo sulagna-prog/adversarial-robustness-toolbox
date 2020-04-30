2ecfb9bf1da74baef308fc00f4607a10b6ff39ed
Beat Buesser <beat.buesser@ie.ibm.com>
2020-05-01 00:35:15 +0100

    Update detector module name
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- tests/defences/detector/poison/test_activation_defence.py
+++ tests/defences/detector/poison/test_activation_defence.py
@@ -22,7 +22,7 @@ import unittest
 
 import numpy as np
 
-from art.defences.detection.poison import ActivationDefence
+from art.defences.detector.poison import ActivationDefence
 from art.utils import load_mnist
 
 from tests.utils import master_seed
